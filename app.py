edc4e52154db06e7c00b3b70674d9b6a1907f804
rmanaem <rmanaem@protonmail.ch>
2022-02-03 14:52:29 -0500

    Implemented upload component

--- app.py
+++ app.py
@@ -12,7 +12,28 @@ app.layout = html.Div([
     html.H1(children='Dashboard',
             style={
                 'textAlign': 'center'
-            })
+            }),
+    html.Br(),
+    html.Br(),
+
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        multiple=True
+    ),
 ])
 
 
