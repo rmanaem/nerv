2989d0329200d60704b11f85a3f3014fde33990d
rmanaem <rmanaem@protonmail.ch>
2022-02-09 01:31:46 -0500

    Added conditions to handle missing fields

--- app.py
+++ app.py
@@ -55,11 +55,11 @@ def process_data(contents):
     data = str(parse_contents(contents).decode('utf8').replace("\'", '\"'))
     data = json.loads(data)
     fsl = [(k, 'FSL', data[k]['FSL']['Result']['result']) for k in data.keys()]
-    fsl = [(i[0], i[1], 0) if i[2] == None else (
+    fsl = [(i[0], i[1], -1) if i[2] == None else (
         i[0], i[1], float(i[2])) for i in fsl]
     freesurfer = [(k, 'FreeSurfer', data[k]['FreeSurfer']
                    ['Result']['result']) for k in data.keys()]
-    freesurfer = [(i[0], i[1], 0) if i[2] == None else (
+    freesurfer = [(i[0], i[1], -1) if i[2] == None else (
         i[0], i[1], float(i[2])) for i in freesurfer]
     x = fsl + freesurfer
     # data for info panel
@@ -92,14 +92,15 @@ def process_click(clickData):
     subject = "Subject: " + \
         clickData['points'][0]['customdata'][0]
     pipeline = "Pipeline: " + clickData['points'][0]['y']
-    result = "Result: " + str(clickData['points'][0]['x'])
+    result = "n/a" if clickData['points'][0]['x'] == - \
+        1 else "Result: " + str(clickData['points'][0]['x'])
     info = [subject, html.Br(), pipeline, html.Br(), result, html.Br()]
     for k, v in list(clickData['points'][0]['customdata'][2].items())[:-1]:
-        status = v['status']
-        inp = str(v['inputID'])
-        out = str(v['outputID'])
-        task = str(v['taskID'])
-        config = str(v['toolConfigID'])
+        status = "Incomplete" if v['status'] == None else v['status']
+        inp = "n/a" if v['inputID'] == None else str(v['inputID'])
+        out = "n/a" if v['outputID'] == None else str(v['outputID'])
+        task = "n/a" if v['taskID'] == None else str(v['taskID'])
+        config = "n/a" if v['toolConfigID'] == None else str(v['toolConfigID'])
         step = html.Details(children=[html.Summary(k), "Status: " + status, html.Br(), "Input ID: " + inp,
                                       html.Br(), "Output ID: " + out, html.Br(), "Task ID " + task, html.Br(), "Tool Configuration ID: " + config])
         info.append(step)
