9e25c0c683982de61447837b7fd9277e8f0fdac9
rmanaem <rmanaem@protonmail.ch>
2022-02-09 01:54:09 -0500

    Fixed the mismatch in data fields

--- app.py
+++ app.py
@@ -54,22 +54,17 @@ def process_data(contents):
     # Assuming the data was in json format
     data = str(parse_contents(contents).decode('utf8').replace("\'", '\"'))
     data = json.loads(data)
-    fsl = [(k, 'FSL', data[k]['FSL']['Result']['result']) for k in data.keys()]
-    fsl = [(i[0], i[1], -1) if i[2] == None else (
-        i[0], i[1], float(i[2])) for i in fsl]
-    freesurfer = [(k, 'FreeSurfer', data[k]['FreeSurfer']
-                   ['Result']['result']) for k in data.keys()]
-    freesurfer = [(i[0], i[1], -1) if i[2] == None else (
-        i[0], i[1], float(i[2])) for i in freesurfer]
+    fsl = [(k, 'FSL', data[k]['FSL']['Result']['result'], data[k]['FSL'])
+           for k in data.keys()]
+    fsl = [(i[0], i[1], -1, i[3]) if i[2] ==
+           None else (i[0], i[1], float(i[2]), i[3]) for i in fsl]
+    freesurfer = [(k, 'FreeSurfer', data[k]['FreeSurfer']['Result']
+                   ['result'], data[k]['FreeSurfer']) for k in data.keys()]
+    freesurfer = [(i[0], i[1], -1, i[3]) if i[2] ==
+                  None else (i[0], i[1], float(i[2]), i[3]) for i in freesurfer]
     x = fsl + freesurfer
-    # data for info panel
-    info = []
-    for k, v in data.items():
-        for i in v.keys():
-            info.append(v[i])
-    # info = [v[i] for i in v.keys() for k, v in data.items()]
     df = pd.DataFrame({'Subject': [i[0] for i in x], 'Pipeline': [
-                      i[1] for i in x], 'Result': [i[2] for i in x], 'Info': info})
+                      i[1] for i in x], 'Result': [i[2] for i in x], 'Info': [i[3] for i in x]})
     return df
 
 
@@ -92,7 +87,7 @@ def process_click(clickData):
     subject = "Subject: " + \
         clickData['points'][0]['customdata'][0]
     pipeline = "Pipeline: " + clickData['points'][0]['y']
-    result = "n/a" if clickData['points'][0]['x'] == - \
+    result = "Result: n/a" if clickData['points'][0]['x'] == - \
         1 else "Result: " + str(clickData['points'][0]['x'])
     info = [subject, html.Br(), pipeline, html.Br(), result, html.Br()]
     for k, v in list(clickData['points'][0]['customdata'][2].items())[:-1]:
@@ -102,7 +97,7 @@ def process_click(clickData):
         task = "n/a" if v['taskID'] == None else str(v['taskID'])
         config = "n/a" if v['toolConfigID'] == None else str(v['toolConfigID'])
         step = html.Details(children=[html.Summary(k), "Status: " + status, html.Br(), "Input ID: " + inp,
-                                      html.Br(), "Output ID: " + out, html.Br(), "Task ID " + task, html.Br(), "Tool Configuration ID: " + config])
+                                      html.Br(), "Output ID: " + out, html.Br(), "Task ID: " + task, html.Br(), "Tool Configuration ID: " + config])
         info.append(step)
 
     return info
