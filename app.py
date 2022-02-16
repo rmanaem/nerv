b1f22d0a4b679399c48a4ac7d143dfdee1f26d0a
rmanaem <rmanaem@protonmail.ch>
2022-02-15 19:44:12 -0500

    Modified the code to automate parsing - process_data now parses data based on the sample data structure regardless of the number of pipelines - generate_summary is no longer hard coded and will give summary regardless of the number of pipelines

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import base64
-from turtle import width
+from re import X
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -66,15 +66,21 @@ def process_data(contents):
     # Assuming the data was in json format
     data = str(parse_contents(contents).decode('utf8').replace("\'", '\"'))
     data = json.loads(data)
-    fsl = [(k, 'FSL', data[k]['FSL']['Result']['result'], data[k]['FSL'])
-           for k in data.keys()]
-    fsl = [(i[0], i[1], -1, i[3]) if i[2] ==
-           None else (i[0], i[1], float(i[2]), i[3]) for i in fsl]
-    freesurfer = [(k, 'FreeSurfer', data[k]['FreeSurfer']['Result']
-                   ['result'], data[k]['FreeSurfer']) for k in data.keys()]
-    freesurfer = [(i[0], i[1], -1, i[3]) if i[2] ==
-                  None else (i[0], i[1], float(i[2]), i[3]) for i in freesurfer]
-    x = fsl + freesurfer
+    # fsl = [(k, 'FSL', data[k]['FSL']['Result']['result'], data[k]['FSL'])
+    #        for k in data.keys()]
+    # fsl = [(i[0], i[1], -1, i[3]) if i[2] ==
+    #        None else (i[0], i[1], float(i[2]), i[3]) for i in fsl]
+    # freesurfer = [(k, 'FreeSurfer', data[k]['FreeSurfer']['Result']
+    #                ['result'], data[k]['FreeSurfer']) for k in data.keys()]
+    # freesurfer = [(i[0], i[1], -1, i[3]) if i[2] ==
+    #               None else (i[0], i[1], float(i[2]), i[3]) for i in freesurfer]
+    # x = fsl + freesurfer
+    x = []
+    for k in data.keys():
+        for v in data[k].keys():
+            x.append((k, v, data[k][v]['Result']['result'], data[k][v]))
+    x = [(i[0], i[1], -1, i[3]) if i[2] ==
+         None else (i[0], i[1], float(i[2]), i[3]) for i in x]
     df = pd.DataFrame({'Subject': [i[0] for i in x], 'Pipeline': [
                       i[1] for i in x], 'Result': [i[2] for i in x], 'Info': [i[3] for i in x]})
     return df
@@ -100,11 +106,18 @@ def generate_summary(contents):
     df = process_data(contents)
     total = str(df.shape[0])
     miss = str(df[df['Result'] == -1].shape[0])
-    fsl = str(df[(df['Pipeline'] == 'FSL') & (df['Result'] == -1)].shape[0])
-    freesurfer = str(df[(df['Pipeline'] == 'FreeSurfer')
-                     & (df['Result'] == -1)].shape[0])
-    return ["Total number of datapoints: " + total, html.Br(), "Total number of missing datapoints: "
-            + miss, html.Br(), "FSL: " + fsl, html.Br(), "FreeSurfer:" + freesurfer]
+    summary = ["Total number of datapoints: " + total, html.Br(), "Total number of missing datapoints: "
+               + miss, html.Br()]
+    pipelines = df['Pipeline'].unique().tolist()
+    for p in pipelines:
+        s = p + ': ' + str(df[(df['Pipeline'] == p) &
+                              (df['Result'] == -1)].shape[0])
+        summary.append(s)
+        summary.append(html.Br())
+    # fsl = str(df[(df['Pipeline'] == 'FSL') & (df['Result'] == -1)].shape[0])
+    # freesurfer = str(df[(df['Pipeline'] == 'FreeSurfer')
+    #                  & (df['Result'] == -1)].shape[0])
+    return summary
 
 
 @app.callback(
@@ -119,6 +132,7 @@ def process_click(clickData):
     result = "Result: N/A" if clickData['points'][0]['x'] == - \
         1 else "Result: " + str(clickData['points'][0]['x'])
     info = [subject, html.Br(), pipeline, html.Br(), result, html.Br()]
+
     for k, v in list(clickData['points'][0]['customdata'][2].items())[:-1]:
         status = "Incomplete" if v['status'] == None else v['status']
         inp = "N/A" if v['inputID'] == None else str(v['inputID'])
