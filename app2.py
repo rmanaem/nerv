1a9219ede9a76922e25f2787f098e570d3e0b4b2
rmanaem <rmanaem@protonmail.ch>
2022-03-08 22:04:46 -0500

    Modified app2.py Removed the upload component and made adjustments in the code.

--- app2.py
+++ app2.py
@@ -10,11 +10,19 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import json
 import dash_bootstrap_components as dbc
+import utility as util
 # For latex
 import dash_defer_js_import as dji
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 port = 7777
+
+files = util.pull_files('./data')
+dfs = []
+for i in files:
+    dfs.append(util.process_file(i))
+df = pd.concat(dfs)
+
 
 # For latex
 app.index_string = '''
@@ -63,31 +71,19 @@ app.layout = html.Div([
 
     html.Br(),
     html.Br(),
-
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or Select Files'
-        ]),
-        style={
-            'width': '20%',
-            'height': '80px',
-            'lineHeight': '80px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': 'auto'
-        },
-    ),
-
     html.Br(),
 
     html.Div([
-        html.Div(id='plot-div',
+        html.Div(dcc.Graph(id='histogram',
+                           figure=px.histogram(df[df['Result'] != -1], x='Result', color='Pipeline',
+                                               barmode='overlay', marginal='rug', hover_data=df.columns).update_layout(
+                               xaxis_title=r'$\text {Hippocampus Volume } (mm^3)$', yaxis_title='Count',
+                               template='plotly_dark'),
+                           config={'displaylogo': False}, style={'height': 760}), id='histogram-div',
                  style={'display': 'inline-block', 'width': '75%'}),
         html.Div(
-            [html.Div(id='summary-div'), html.Br(), html.Div(id='info-div')],
+            [html.Div(util.generate_summary(df), id='summary-div'),
+             html.Br(), html.Div(id='info-div')],
             style={'width': '25%', 'margin-left': '30px'}
         )
     ],
@@ -95,7 +91,7 @@ app.layout = html.Div([
         'display': 'flex'
     }),
 
-    html.Div(id="scatter-matrix-div")
+    html.Div(util.plot_scatters(df), id="scatter-matrix-div")
 
 ])
 
@@ -118,47 +114,46 @@ def process_data(contents):
     return df
 
 
-@app.callback(
-    Output(component_id='plot-div', component_property='children'),
-    Input(component_id='upload-data', component_property='contents'))
-def plot_data(contents):
-    if not contents:
-        return dash.no_update
-    df = process_data(contents)
-    fig = px.histogram(df[df['Result'] != -1], x='Result', color='Pipeline',
-                       barmode='overlay', marginal='rug', hover_data=df.columns).update_layout(
-                           xaxis_title=r'$\text {Hippocampus Volume } (mm^3)$', yaxis_title='Count', template='plotly_dark')
-    return dcc.Graph(id='plot', figure=fig, config={'displaylogo': False}, style={'height': 760})
+# @app.callback(
+#     Output(component_id='histogram-div', component_property='children'),
+#     Input(component_id='upload-data', component_property='contents'))
+# def plot_histogram(contents):
+#     if not contents:
+#         return dash.no_update
+#     df = process_data(contents)
+#     fig = px.histogram(df[df['Result'] != -1], x='Result', color='Pipeline',
+#                        barmode='overlay', marginal='rug', hover_data=df.columns).update_layout(
+#                            xaxis_title=r'$\text {Hippocampus Volume } (mm^3)$', yaxis_title='Count', template='plotly_dark')
+#     return dcc.Graph(id='histogram', figure=fig, config={'displaylogo': False}, style={'height': 760})
 
 
-@app.callback(Output('summary-div', 'children'),
-              Input('upload-data', 'contents'))
-def generate_summary(contents):
-    if not contents:
-        return dash.no_update
-    df = process_data(contents)
-    total = str(df.shape[0])
-    miss = str(df[df['Result'] == -1].shape[0])
-    header = html.H4('Summary', style={'textAlign': 'center'})
-    summary = [header, "Total number of datapoints: " + total, html.Br(), "Total number of missing datapoints: "
-               + miss, html.Br()]
-    pipelines = df['Pipeline'].unique().tolist()
-    for p in pipelines:
-        s = p + ': ' + str(df[(df['Pipeline'] == p) &
-                              (df['Result'] == -1)].shape[0])
-        summary.append(s)
-        summary.append(html.Br())
-    return html.Div(html.P(summary, style={'margin-left': '10px'}),
-                    style={
-        'width': '90%',
-        'box-shadow': 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px',
-        'border-radius': '7px',
-        'border': '0.25px solid'})
+# @app.callback(Output('summary-div', 'children'))
+# def generate_summary(df):
+#     if not df:
+#         return dash.no_update
+#     # df = process_data(contents)
+#     total = str(df.shape[0])
+#     miss = str(df[df['Result'] == -1].shape[0])
+#     header = html.H4('Summary', style={'textAlign': 'center'})
+#     summary = [header, "Total number of datapoints: " + total, html.Br(), "Total number of missing datapoints: "
+#                + miss, html.Br()]
+#     pipelines = df['Pipeline'].unique().tolist()
+#     for p in pipelines:
+#         s = p + ': ' + str(df[(df['Pipeline'] == p) &
+#                               (df['Result'] == -1)].shape[0])
+#         summary.append(s)
+#         summary.append(html.Br())
+#     return html.Div(html.P(summary, style={'margin-left': '10px'}),
+#                     style={
+#         'width': '90%',
+#         'box-shadow': 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px',
+#         'border-radius': '7px',
+#         'border': '0.25px solid'})
 
 
 @app.callback(
     Output('info-div', 'children'),
-    Input('plot', 'clickData'))
+    Input('histogram', 'clickData'))
 def process_click(clickData):
     if not clickData:
         return dash.no_update
@@ -191,25 +186,25 @@ def process_click(clickData):
         'border': '0.25px solid'})
 
 
-@app.callback(Output('scatter-matrix-div', 'children'),
-              Input('upload-data', 'contents'))
-def scatter_matrix(contents):
-    if not contents:
-        return dash.no_update
-    df1 = process_data(contents)
-    df = pd.DataFrame()
-    pipelines = df1['Pipeline'].unique().tolist()
-    df['Subject'] = df1['Subject'].unique()
-    for i in pipelines:
-        df[i] = df1[df1['Pipeline'] == i]['Result'].reset_index(drop=True)
-    plots = []
-    for i, j in enumerate(pipelines):
-        for z, w in enumerate(pipelines):
-            if j != w:
-                scatter = px.scatter(
-                    df, x=j, y=w, marginal_x='histogram', marginal_y='histogram')
-                plots.append(dcc.Graph(figure=scatter))
-    return plots
+# @app.callback(Output('scatter-matrix-div', 'children'),
+#               Input('upload-data', 'contents'))
+# def scatter_matrix(contents):
+#     if not contents:
+#         return dash.no_update
+#     df1 = process_data(contents)
+#     df = pd.DataFrame()
+#     pipelines = df1['Pipeline'].unique().tolist()
+#     df['Subject'] = df1['Subject'].unique()
+#     for i in pipelines:
+#         df[i] = df1[df1['Pipeline'] == i]['Result'].reset_index(drop=True)
+#     plots = []
+#     for i, j in enumerate(pipelines):
+#         for z, w in enumerate(pipelines):
+#             if j != w:
+#                 scatter = px.scatter(
+#                     df, x=j, y=w, marginal_x='histogram', marginal_y='histogram')
+#                 plots.append(dcc.Graph(figure=scatter))
+#     return plots
 
 
 if __name__ == '__main__':
