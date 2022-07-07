93e771c5058ca5df38af459937312ef55e518841
Arman <rmanaem@protonmail.ch>
2022-07-07 19:23:57 -0400

    Implemented single parameter for the start function When single is set to false the app will start in multiple mode otherwise it will start  in single mode

--- nerv/app.py
+++ nerv/app.py
@@ -1,17 +1,13 @@
 import dash
+from dash import html
 import pandas as pd
 import dash_bootstrap_components as dbc
 from nerv import utility as util
-from nerv import callbacks
 from nerv import layouts
+from nerv import callbacks
 
 
-def start(path, local=True):
-    files = util.pull_files(path)
-    dfs = []
-    for i, j in enumerate(files):
-        dfs.append(util.process_file(j, i))
-    df = pd.concat(dfs)
+def start(path, local=True, single=True):
 
     dbc_css = (
         "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
@@ -20,14 +16,27 @@ def start(path, local=True):
     template2 = "simple_white"
     theme1 = dbc.themes.DARKLY
     theme2 = dbc.themes.FLATLY
-    app = dash.Dash(__name__, external_stylesheets=[theme1, dbc_css])
+    app = dash.Dash(__name__, routes_pathname_prefix='/',
+                    external_stylesheets=[theme1, dbc_css])
     app.title = 'NeRV'
+    if single:
+        files = util.pull_files(path)
+        dfs = []
+        for i, j in enumerate(files):
+            dfs.append(util.process_file(j, i))
+        df = pd.concat(dfs)
 
-    app.layout = layouts.single(df, theme1, theme2, template1)
+        app.layout = layouts.single(df, theme1, theme2, template1)
 
-    callbacks.single(df, template1, template2)
+        callbacks.single(df, template1, template2)
+    else:
+        experiments = util.pull_directories(path)
+        layout = layouts.multiple(path, theme1, theme2, template1)
+        app.layout = layout[0]
+        app.validation_layout = html.Div(layout)
+        callbacks.multiple(path, theme1, theme2, template1, template2)
 
     if local:
-        app.run_server()
+        app.run_server(debug=True)
     else:
         return app.server
