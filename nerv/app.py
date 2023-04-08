d80125b4cc6f0a73e836ba323559737a47b7b112
Arman <rmanaem@protonmail.ch>
2023-04-08 18:57:15 -0400

    Fixed the live update issue
    - Removed multiple option and its related code
    - Implemented `serve_layout` function
    - Refactored code

--- nerv/app.py
+++ nerv/app.py
@@ -1,14 +1,13 @@
 import dash
-from dash import html, dcc
-import pandas as pd
 import dash_bootstrap_components as dbc
-from nerv import utility as util
-from nerv import layouts
-from nerv import callbacks
+import pandas as pd
+
+from nerv.callbacks import callbacks
+from nerv.layouts import layout
+from nerv.utility import process_file, pull_files
 
 
-def start(path, local=True, single=True):
-
+def start(path, local=True):
     dbc_css = (
         "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
     )
@@ -16,25 +15,26 @@ def start(path, local=True, single=True):
     template2 = "simple_white"
     theme1 = dbc.themes.DARKLY
     theme2 = dbc.themes.FLATLY
-    app = dash.Dash(__name__, routes_pathname_prefix='/',
-                    external_stylesheets=[theme1, dbc_css])
-    app.title = 'NeRV'
-    if single:
-        files = util.pull_files(path)
+    app = dash.Dash(
+        __name__, routes_pathname_prefix="/", external_stylesheets=[theme1, dbc_css]
+    )
+    app.title = "NeRV"
+
+    def serve_layout():
+        files = pull_files(path)
         dfs = []
         for i, j in enumerate(files):
-            dfs.append(util.process_file(j, i))
+            dfs.append(process_file(j, i))
+        global df
         df = pd.concat(dfs)
 
-        app.layout = layouts.single(df, theme1, theme2, template1)
+        return layout(df, theme1, theme2, template1)
 
-        callbacks.single(df, template1, template2)
-    else:
-        app.layout = html.Div(layouts.multiple(path, theme1, theme2, template1)[0])
-        app.validation_layout = html.Div(layouts.multiple(path, theme1, theme2, template1))
-        callbacks.multiple(path, theme1, theme2, template1, template2)
+    app.layout = serve_layout
+
+    callbacks(df, template1, template2)
 
     if local:
-        app.run_server(debug=True)
+        app.run_server()
     else:
         return app.server
