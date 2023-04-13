a53a46d951cfe32929a5e4acd028ad4f5118096e
Arman <rmanaem@protonmail.ch>
2023-04-13 18:53:43 -0400

    Implemented `process_files` function
    `process_files` will use `pull_files` and `process_file`
    functions to prcess files in the input path

--- nerv/utility.py
+++ nerv/utility.py
@@ -1,43 +1,68 @@
-import os
 import json
-import pandas as pd
-from dash import html
-import plotly.express as px
+import os
 
-colors = [px.colors.qualitative.G10, px.colors.sequential.Teal, px.colors.sequential.Brwnyl,
-          px.colors.sequential.Burg, px.colors.sequential.Purp]
+import pandas as pd
+import plotly.express as px
+from dash import html
+
+colors = [
+    px.colors.qualitative.G10,
+    px.colors.sequential.Teal,
+    px.colors.sequential.Brwnyl,
+    px.colors.sequential.Burg,
+    px.colors.sequential.Purp,
+]
 
 
 def pull_files(path):
     files = []
     for i in os.listdir(path):
-        files.append((path + '/' + i, i[:len(i)-5]))
+        files.append((path + "/" + i, i[: len(i) - 5]))
     return files
 
 
 def process_file(file, color):
     data = None
-    with open(file[0], 'r') as dataset:
+    with open(file[0], "r") as dataset:
         data = json.load(dataset)
     x = []
     for k in data.keys():
         z = len(colors[color]) - 1
         for v in data[k].keys():
-            x.append((k, v, data[k][v]['Result']['result'],
-                     data[k][v], colors[color][z]))
+            x.append(
+                (k, v, data[k][v]["Result"]["result"], data[k][v], colors[color][z])
+            )
             z -= 1
-    x = [(i[0], i[1], -1, i[3], i[4]) if i[2] ==
-         None else (i[0], i[1], float(i[2]), i[3], i[4]) for i in x]
-    df = pd.DataFrame({'Subject': [i[0] for i in x], 'Dataset-Pipeline': [
-                      file[1]+'-'+i[1] for i in x], 'Result': [i[2] for i in x], 'Info': [i[3] for i in x], 'Color': [i[4] for i in x]})
+    x = [
+        (i[0], i[1], -1, i[3], i[4])
+        if i[2] is None
+        else (i[0], i[1], float(i[2]), i[3], i[4])
+        for i in x
+    ]
+    df = pd.DataFrame(
+        {
+            "Subject": [i[0] for i in x],
+            "Dataset-Pipeline": [file[1] + "-" + i[1] for i in x],
+            "Result": [i[2] for i in x],
+            "Info": [i[3] for i in x],
+            "Color": [i[4] for i in x],
+        }
+    )
     return df
+
+
+def process_files(path):
+    files = pull_files(path)
+    dfs = []
+    for i, j in enumerate(files):
+        dfs.append(process_file(j, i))
+    return pd.concat(dfs)
 
 
 def pull_directories(path):
     files = []
     for directory in os.listdir(path):
-        files.append((directory, pull_files(
-            os.path.join(path, directory)), []))
+        files.append((directory, pull_files(os.path.join(path, directory)), []))
     for i in files:
         for z, w in enumerate(i[1]):
             i[2].append(process_file(w, z))
@@ -46,19 +71,30 @@ def pull_directories(path):
 
 def generate_summary(df):
     total = str(df.shape[0])
-    miss = str(df[df['Result'] == -1].shape[0])
-    header = html.H4('Summary', style={'textAlign': 'center'})
-    summary = [header, "Total number of datapoints: " + total, html.Br(), "Total number of missing datapoints: "
-               + miss, html.Br()]
-    pipelines = df['Dataset-Pipeline'].unique().tolist()
+    miss = str(df[df["Result"] == -1].shape[0])
+    header = html.H4("Summary", style={"textAlign": "center"})
+    summary = [
+        header,
+        "Total number of datapoints: " + total,
+        html.Br(),
+        "Total number of missing datapoints: " + miss,
+        html.Br(),
+    ]
+    pipelines = df["Dataset-Pipeline"].unique().tolist()
     for p in pipelines:
-        s = p + ': ' + str(df[(df['Dataset-Pipeline'] == p) &
-                              (df['Result'] == -1)].shape[0])
+        s = (
+            p
+            + ": "
+            + str(df[(df["Dataset-Pipeline"] == p) & (df["Result"] == -1)].shape[0])
+        )
         summary.append(s)
         summary.append(html.Br())
-    return html.Div(html.P(summary, style={'margin-left': '10px'}),
-                    style={
-        'width': '90%',
-        'box-shadow': 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px',
-        'border-radius': '7px',
-        'border': '0.25px solid'})
+    return html.Div(
+        html.P(summary, style={"margin-left": "10px"}),
+        style={
+            "width": "90%",
+            "box-shadow": "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
+            "border-radius": "7px",
+            "border": "0.25px solid",
+        },
+    )
