a8962d6306d855156605833549ac7b71224448d9
Arman <rmanaem@protonmail.ch>
2023-04-13 11:34:54 -0400

    Implemented callback functions
    The callbacks inside app.py module rely on functions inside
    callbacks.py module for their logic

--- nerv/app.py
+++ nerv/app.py
@@ -1,8 +1,14 @@
 import dash
 import dash_bootstrap_components as dbc
 import pandas as pd
+from dash import Input, Output, callback
+from dash_bootstrap_templates import ThemeSwitchAIO
 
-from nerv import callbacks
+from nerv.callbacks import (
+    histogram_click_func,
+    scatter_click_func,
+    switch_template_func,
+)
 from nerv.layouts import layout
 from nerv.utility import process_file, pull_files
 
@@ -32,7 +38,32 @@ def start(path, local=True):
 
     app.layout = serve_layout
 
+    @callback(
+        Output("histogram", "figure"),
+        Output("scatter", "figure"),
+        Input(ThemeSwitchAIO.ids.switch("theme"), "value"),
+        Input("histogram", "figure"),
+        Input("scatter", "figure"),
+    )
+    def switch_template(value, histogram_fig, scatter_fig):
+        return switch_template_func(
+            value, histogram_fig, scatter_fig, template1, template2
+        )
+
+    @callback(Output("info-div", "children"), Input("histogram", "clickData"))
+    def process_click(clickData):
+        return histogram_click_func(clickData)
+
+    @callback(
+        Output("info-div-scatter", "children"),
+        Input("scatter", "clickData"),
+        Input("x", "value"),
+        Input("y", "value"),
+    )
+    def process_click_scatter(clickData, x, y):
+        return scatter_click_func(clickData, x, y, df)
+
     if local:
-        app.run_server()
+        app.run_server(debug=True)
     else:
         return app.server
