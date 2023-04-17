2b9fb827d324873887af5108297737ff29c8fc42
Arman <rmanaem@protonmail.ch>
2023-04-17 17:30:39 -0400

    Removed `test_switch_template_func` test

--- tests/test_app_callbacks.py
+++ tests/test_app_callbacks.py
@@ -1,15 +1,1 @@
-import plotly.io as pio
 import pytest
-
-from nerv.callbacks import switch_template_func
-
-
-@pytest.mark.parametrize(
-    "switch, template1, template2",
-    [(True, "plotly_dark", "simple_white"), (False, "plotly_dark", "simple_white")],
-)
-def test_switch_template_fuc(switch, histogram, scatter, template1, template2):
-    template = template1 if switch else template2
-    output = switch_template_func(switch, histogram, scatter, template1, template2)
-    assert output[0]["layout"]["template"] == pio.templates[template]
-    assert output[1]["layout"]["template"] == pio.templates[template]
