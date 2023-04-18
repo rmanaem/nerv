bee24d3f7eaeaa58de5fc6da9542357204a2e14f
Arman <rmanaem@protonmail.ch>
2023-04-18 13:40:03 -0400

    Implemented `test_get_metadata` test

--- tests/test_app_callbacks.py
+++ tests/test_app_callbacks.py
@@ -1,1 +1,16 @@
 import pytest
+
+from nerv.utility import get_metadata
+
+
+@pytest.mark.parametrize(
+    "clickData, metadata, x, y",
+    [
+        ("histogram", "histogram", "prevent-AD-FSL", None),
+        ("histogram", "histogram", None, "prevent-AD-FreeSurfer"),
+        ("scatter", "scatter", "prevent-AD-FSL", "prevent-AD-FreeSurfer"),
+    ],
+    indirect=["clickData", "metadata"],
+)
+def test_get_metadata(clickData, metadata, x, y, df):
+    assert metadata == get_metadata(clickData, x, y, df)
