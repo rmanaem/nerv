57261813e33e674af94abe0c958ee195f27b4ea3
Arman <rmanaem@protonmail.ch>
2023-05-14 16:58:09 -0400

    Added DBC_THEMES

--- nerv/utility.py
+++ nerv/utility.py
@@ -15,6 +15,34 @@ COLORS = [
     px.colors.sequential.Purp,
 ]
 
+DBC_THEMES = {
+    "BOOTSTRAP": dbc.themes.BOOTSTRAP,
+    "CERULEAN": dbc.themes.CERULEAN,
+    "COSMO": dbc.themes.COSMO,
+    "CYBORG": dbc.themes.CYBORG,
+    "DARKLY": dbc.themes.DARKLY,
+    "FLATLY": dbc.themes.FLATLY,
+    "JOURNAL": dbc.themes.JOURNAL,
+    "LITERA": dbc.themes.LITERA,
+    "LUMEN": dbc.themes.LUMEN,
+    "LUX": dbc.themes.LUX,
+    "MATERIA": dbc.themes.MATERIA,
+    "MINTY": dbc.themes.MINTY,
+    "MORPH": dbc.themes.MORPH,
+    "PULSE": dbc.themes.PULSE,
+    "SANDSTONE": dbc.themes.SANDSTONE,
+    "SIMPLEX": dbc.themes.SIMPLEX,
+    "SKETCHY": dbc.themes.SKETCHY,
+    "SLATE": dbc.themes.SLATE,
+    "SOLAR": dbc.themes.SOLAR,
+    "SPACELAB": dbc.themes.SPACELAB,
+    "SUPERHERO": dbc.themes.SUPERHERO,
+    "UNITED": dbc.themes.UNITED,
+    "VAPOR": dbc.themes.VAPOR,
+    "YETI": dbc.themes.YETI,
+    "ZEPHYR": dbc.themes.ZEPHYR,
+}
+
 
 def pull_files(path):
     """
